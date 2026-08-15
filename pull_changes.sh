8d524cdbf3908c1b27215eeb72f6df4850da65f5
solovmikebmstu <solovmikebmstu@gmail.com>
2026-08-15 14:44:29 +0300

    add pull_cahnges.sh

---
+++ pull_changes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp ~/.vimrc ./
+cp ~/.vim/colors/ayu.vim ./.vim/colors/
+
+cp ~/.bashrc ./ 
+
+cp ~/.config/kitty/kitty.conf ./.config/kitty
+cp ~/.config/kitty/current-theme.conf ./.config/kitty
+
